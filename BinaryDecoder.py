fd9a0b7742188119cd494bbfef9ddd1758799de2
Mikerah <Mikerah1@users.nonreply.github.com>
2015-07-30 19:53:19 -0400

    Modified translator and added decoder

--- BinaryDecoder.py
+++ BinaryDecoder.py
@@ -2,9 +2,9 @@ import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QTextEdit,QLabel, \
 QPushButton, QVBoxLayout, QShortcut
 from PyQt5.QtGui import QKeySequence
-from binary import toBinFrStr
+from binary import toStrFrBin
 
-class BinaryTranslator(QWidget):
+class BinaryDecoder(QWidget):
 
     def __init__(self):
         super().__init__()
@@ -12,10 +12,10 @@ class BinaryTranslator(QWidget):
         self.initUI()
         
     def initUI(self):
-        self.textTitle = QLabel('Text to Translate')
+        self.textTitle = QLabel('Text to Decode')
         self.textEdit = QTextEdit()
         
-        self.binaryTitle = QLabel('Translated text')
+        self.binaryTitle = QLabel('Decoded text')
         self.binaryEdit = QTextEdit()
         
         self.translateButton = QPushButton('Translate', self)
@@ -27,25 +27,24 @@ class BinaryTranslator(QWidget):
         self.vbox.addWidget(self.binaryTitle)
         self.vbox.addWidget(self.binaryEdit)
         self.vbox.addWidget(self.translateButton)
-        
 
-        self.setLayout(self.vbox)
-        
         self.binaryCopy = QShortcut(QKeySequence("Ctrl-C"), self.binaryEdit)
         self.binaryPaste = QShortcut(QKeySequence("Ctrl-V"), self.binaryEdit)
         self.textCopy = QShortcut(QKeySequence("Ctrl-C"), self.textEdit)
-        self.textPaste = QShortcut(QKeySequence("Ctrl-V"), self.textEdit)
+        self.textPaste = QShortcut(QKeySequence("Ctrl-V"), self.textEdit)         
+
+        self.setLayout(self.vbox)
     
         self.setGeometry(300,300,550,600)
-        self.setWindowTitle('Binary Translator')
+        self.setWindowTitle('Binary Decoder')
         self.show()
 
     def translateData(self):
         text = self.textEdit.toPlainText()
-        self.binaryEdit.setText(toBinFrStr(str(text)))
-       
-    def keyPresEvent(self, k):
+        self.binaryEdit.setText(toStrFrBin(str(text)))
         
+    def keyPressEvent(self, k):
+    
         if k.key() == self.binaryCopy:
             binaryEdit.copy(self)
 
@@ -56,10 +55,9 @@ class BinaryTranslator(QWidget):
             textEdit.copy(self)
 
         elif k.key() == self.textPaste:
-            textEdit.paste(self)        
-            
-if __name__ == '__main__':
+            textEdit.paste(self)
 
+if __name__ == '__main__':            
     app = QApplication(sys.argv)
-    bin = BinaryTranslator()
+    bin = BinaryDecoder()
     sys.exit(app.exec_())
